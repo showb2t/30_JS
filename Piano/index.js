559bca55aaf49512fc2ce5addb9fb198aaadb0a3
shobhit <shobhit.m16@iiits.in>
2023-01-12 02:49:58 +0530

    final build ?

--- Piano/index.js
+++ Piano/index.js
@@ -1,10 +1,25 @@
+function invertHex(hex) {
+  return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+}
+
 window.addEventListener('keydown', function(e){
-		console.log(`${e.keyCode}`);
 		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 		const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-		console.log(key);
-		if (~audio) {return};
+		if (!audio) return;
 		audio.currentTime = 0;
 		audio.play();
+		if (!key) return;
 		key.classList.add('playing');
+		color = Math.floor(Math.random() * 14562358).toString(16);
+		key.style.backgroundColor = "#"+ color;
+		key.innerHTML = `${e.keyCode}`;
+		key.style.color = "#"+invertHex(color);
 	});
+
+function removeTransition(e){
+	if (e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition ));
